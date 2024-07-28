a1e630302e628313a3825e8df13020fcc62b23d5
Simran Vinodkumar Ahirwar <70149684+simran0809@users.noreply.github.com>
2024-07-28 22:43:09 +0530

    Add files via upload

---
+++ Quick Sort.c
@@ -0,0 +1,60 @@
+
+//Name : Simran Vinodkumar Ahirwar 
+
+//quick sort 
+#include <stdio.h>
+
+// Function to swap two elements
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+// Function to partition the array on the basis of pivot element
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];  // Pivot
+    int i = (low - 1);  // Index of smaller element
+
+    for (int j = low; j <= high - 1; j++) {
+        // If current element is smaller than the pivot
+        if (arr[j] < pivot) {
+            i++;  // Increment index of smaller element
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+// Function to implement QuickSort
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        // pi is partitioning index, arr[pi] is now at right place
+        int pi = partition(arr, low, high);
+
+        // Separately sort elements before partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// Main function to test the above code
+int main() {
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Given array is \n");
+    printArray(arr, n);
+    quickSort(arr, 0, n - 1);
+    printf("Sorted array is \n");
+    printArray(arr, n);
+    return 0;
+}
